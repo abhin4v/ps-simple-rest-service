c05371b46913cc20cc1d24afd8b71acff61b322d
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-08-28 11:14:37 +0530

    Types + Persistence

---
+++ src/SimpleService/Persistence.purs
@@ -0,0 +1,38 @@
+module SimpleServer.Persistence
+  ( insertUser
+  , findUser
+  , updateUser
+  , deleteUser
+  ) where
+
+import Prelude
+
+import Control.Monad.Aff (Aff)
+import Data.Array as Array
+import Data.Maybe (Maybe)
+import Database.PostgreSQL as PG
+import SimpleServer.Types (User(..), UserID)
+
+insertUserQuery :: String
+insertUserQuery = "insert into users (id, name) values ($1, $2)"
+
+findUserQuery :: String
+findUserQuery = "select id, name from users where id = $1"
+
+updateUserQuery :: String
+updateUserQuery = "update users set name = $1 where id = $2"
+
+deleteUserQuery :: String
+deleteUserQuery = "delete from users where id = $1"
+
+insertUser :: forall eff. PG.Connection -> User -> Aff (postgreSQL :: PG.POSTGRESQL | eff) Unit
+insertUser conn user = PG.execute conn (PG.Query insertUserQuery) user
+
+findUser :: forall eff. PG.Connection -> UserID -> Aff (postgreSQL :: PG.POSTGRESQL | eff) (Maybe User)
+findUser conn userID = map Array.head $ PG.query conn (PG.Query findUserQuery) (PG.Row1 userID)
+
+updateUser :: forall eff. PG.Connection -> User -> Aff (postgreSQL :: PG.POSTGRESQL | eff) Unit
+updateUser conn (User {id, name}) = PG.execute conn (PG.Query updateUserQuery) (PG.Row2 name id)
+
+deleteUser :: forall eff. PG.Connection -> UserID -> Aff (postgreSQL :: PG.POSTGRESQL | eff) Unit
+deleteUser conn userID = PG.execute conn (PG.Query deleteUserQuery) (PG.Row1 userID)
---
+++ src/SimpleService/Types.purs
@@ -0,0 +1,39 @@
+module SimpleServer.Types where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Either (Either(..))
+import Data.Foreign.Class (class Decode, class Encode)
+import Data.Foreign.Generic (defaultOptions, genericDecode, genericEncode)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Database.PostgreSQL (class FromSQLRow, class ToSQLRow, fromSQLValue, toSQLValue)
+
+type UserID = Int
+
+newtype User = User
+  { id   :: UserID
+  , name :: String
+  }
+
+derive instance genericUser :: Generic User _
+
+instance showUser :: Show User where
+  show = genericShow
+
+instance decodeUser :: Decode User where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+instance encodeUser :: Encode User where
+  encode = genericEncode $ defaultOptions { unwrapSingleConstructors = true }
+
+instance userFromSQLRow :: FromSQLRow User where
+  fromSQLRow [id, name] =
+    User <$> ({ id: _, name: _} <$> fromSQLValue id <*> fromSQLValue name)
+
+  fromSQLRow xs = Left $ "Row has " <> show n <> " fields, expecting 2."
+    where n = Array.length xs
+
+instance userToSQLRow :: ToSQLRow User where
+  toSQLRow (User {id, name}) = [toSQLValue id, toSQLValue name]
